e4b0c89bf6925e3ddafa1afba28be1645d384e3c
Vahala12 <phuhanh256@gmail.com>
2026-09-16 18:55:35 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -11,7 +11,7 @@ function login(username, password) {
     throw new Error('Password contains special characters');
   }
 
-  if (username !== 'admin' || password !== '123') {
+  if (username !== 'admin' || password !== '9999') {
     throw new Error('Invalid username or password');
   }
 
